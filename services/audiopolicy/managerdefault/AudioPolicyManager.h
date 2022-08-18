fdaf9979801c2cbb44495c6d76ca4a09fb8913a3
Peter Cai <peter@typeblog.net>
2022-08-18 15:44:46 -0400

    APM: Restore S, R and Q behavior respectively for telephony audio
    This conditionally reverts part of b2e5cb (T), 51c9cc (S) and afd4ce (R)
    when the VNDK version is equal to or before S, R and Q respectively.
    
    On R, commit afd4ce made it so that both HW and SW bridging go through
    `createAudioPatch()`, which is broken on some devices such as on MTK Q
    vendor, because their HAL do not support HW patching via the newer
    `createAudioPatch()` method. Instead, the patching on Q was done through
    `setOutputDevices()`.
    
    On S, commit 51c9cc refactored the related code again such that HW
    bridging for the Rx direction is essentially removed, replaced with SW
    bridging through `startAudioSource()`. This is, again, broken on MTK R
    vendor devices.
    
    On T, commit b2e5cb applied the same SW bridging to the Tx direction.
    
    All of these commits rely on assumptions that are not tested through
    VTS and just presumed to be true. Although we can blame MTK for not
    supporting all the possible cases in their HAL, it will not fix
    anything, and really frameworks code should not depend on such untested
    assumptions.
    
    To work around said issues, we restore old behavior from S, R and Q
    relying on the value of `ro.vndk.version`.
    
    Change-Id: I56d36d2aef4319935cb88a3e4771b23c6d5b2145

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -1089,6 +1089,9 @@ protected:
 
         SoundTriggerSessionCollection mSoundTriggerSessions;
 
+        sp<AudioPatch> mCallTxPatch;
+        sp<AudioPatch> mCallRxPatch;
+
         HwAudioOutputCollection mHwOutputs;
         SourceClientCollection mAudioSources;
 
